4c63a150508289db6803cd64bf1e0cddf50896eb
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-19 01:45:56 +0300

    refactor: Remove jQuery and its use

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -70,79 +70,7 @@
     }).catch(function(e) {
       console.error('Error during service worker registration:', e);
     });
-    window.initMap = function() {};
   }
-
-  // Retrieve API data
-  // https://developer.foursquare.com/docs/venues/explore
-  // https://api.foursquare.com/v2/venues/explore?&venuePhotos=1&client_id=DZLWV3OVTRDIJ4A3HYBWIHZLHNPPWSPULWU0MOIUPV4UGD0B&client_secret=MFMOIF4BD4H0K3ULA41LF2WD5SK0KRJAQLTUZT4VV3XVGC0O&ll=38.464576%2C23.600043&v=20170425
-  const FOURSQUARE_CLIENT_ID = 'DZLWV3OVTRDIJ4A3HYBWIHZLHNPPWSPULWU0MOIUPV4UGD0B';
-  const FOURSQUARE_CLIENT_SECRET = 'MFMOIF4BD4H0K3ULA41LF2WD5SK0KRJAQLTUZT4VV3XVGC0O';
-  var venues = [];
-  $.getJSON('https://api.foursquare.com/v2/venues/explore?', {
-    client_id: FOURSQUARE_CLIENT_ID,
-    client_secret: FOURSQUARE_CLIENT_SECRET,
-    ll: '38.464576,23.600043',
-    v: '20170425',
-    venuePhotos: '1'
-  }, function(data) {
-    for (let item of data.response.groups[0].items) {
-      let venue = {
-        name: item.venue.name,
-        foursquare: 'https://foursquare.com/v/' + item.venue.id + '?ref=' + FOURSQUARE_CLIENT_ID,
-        location: {lat: item.venue.location.lat, lng: item.venue.location.lng},
-        distance: item.venue.location.distance,
-        address: item.venue.location.address,
-        rating: {
-          number: Math.round(item.venue.rating) / 2,
-          stars: [
-            'images/star_border_yellow_18px.svg',
-            'images/star_border_yellow_18px.svg',
-            'images/star_border_yellow_18px.svg',
-            'images/star_border_yellow_18px.svg',
-            'images/star_border_yellow_18px.svg'
-          ]
-        },
-        icon: item.venue.categories[0].icon.prefix + '64' + item.venue.categories[0].icon.suffix,
-        category: {
-          name: item.venue.categories[0].name,
-          tag: item.venue.categories[0].shortName
-        },
-        photo: item.venue.featuredPhotos.items[0].prefix + 'original' + item.venue.featuredPhotos.items[0].suffix,
-      };
-      // fix rating.stars
-      var i;
-      for (i = 0; i < Math.floor(venue.rating.number); i++) {
-        venue.rating.stars[i] = 'images/star_yellow_18px.svg';
-      }
-      if (venue.rating.number % 1 !== 0) {
-        venue.rating.stars[i] = 'images/star_half_yellow_18px.svg';
-      }
-      // filter through non-standard information
-      if (item.venue.contact.phone) {
-        venue.phone = item.venue.contact.phone;
-      }
-      if (item.venue.contact.facebook) {
-        venue.facebook = 'https://www.facebook.com/' + item.venue.contact.facebook;
-      }
-      if (item.venue.price) {
-        venue.price = item.venue.price.tier;
-      }
-      if (item.venue.hours) {
-        venue.isOpen = item.venue.hours.isOpen;
-      }
-      if (item.venue.url) {
-        venue.url = item.venue.url;
-      }
-      venues.push(venue);
-    }
-    console.log(venues);
-  }).fail(function() {
-    // TODO: Add a proper fail message
-  });
-
-  // Initialize markers array from data
-  window.markers = ko.observableArray([]);
   /**
    * Populates a given google Map with given markers array
    * @param {Array} markers Markers Array
