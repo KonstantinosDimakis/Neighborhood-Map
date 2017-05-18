d797a108bc61fafaa1cf15e2cc907be082b32505
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-18 14:00:17 +0300

    feat: Make fake data array to test on venues list

--- app/scripts/viewModel.js
+++ app/scripts/viewModel.js
@@ -1,6 +1,243 @@
+var data = [
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_border_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzouna',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  }
+  ,  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  },
+  {
+    name: 'Zouzhs',
+    distance: '492',
+    rating: {
+      number: 3.5,
+      stars: [
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_black_18px.svg',
+        'images/star_half_black_18px.svg',
+        'images/star_border_black_18px.svg'
+      ]
+    },
+    icon: 'https://ss3.4sqi.net/img/categories_v2/building/gym_32.png',
+    category: {
+      tag: 'Gym'
+    }
+  }
+];
+
 var ViewModel = function() {
   'use strict';
   this.filter = ko.observable('');
+  this.venues = ko.observableArray(data);
 };
 
 ko.applyBindings(new ViewModel());
