6b72196eae905edde9a619fa09aeb39bcdd79b23
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-20 20:27:24 +0300

    style: Fix some ESLint suggestions

--- app/scripts/viewModel.js
+++ app/scripts/viewModel.js
@@ -15,14 +15,20 @@ var ViewModel = function() {
     }
     return ko.utils.arrayFilter(this.venues(), function(venue) {
       // TODO: Make filtering UX MUCH better
-      return venue.category.tag === filter || venue.category.name === filter || venue.name === filter;
+      return venue.category.tag === filter ||
+        venue.category.name === filter ||
+        venue.name === filter;
     });
   }, this);
   dataModel.foursquare.then(data => {
     this.venues(data);
   }, function(error) {
     console.error('Failed:', error);
-    document.body.classList.add('error', 'mdl-color--grey-300', 'mdl-color-text--grey-700');
+    document.body.classList.add(
+      'error',
+      'mdl-color--grey-300',
+      'mdl-color-text--grey-700'
+    );
     document.body.innerHTML = '<h1 class="error__text">Oops, something went wrong</h1>';
   });
 };
