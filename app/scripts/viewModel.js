e025ade25341978f5559f53b6deb658b32e6e531
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-23 16:33:23 +0300

    docs: Add docs & comments

--- app/scripts/viewModel.js
+++ app/scripts/viewModel.js
@@ -2,28 +2,70 @@
 
 var ViewModel = function() {
   'use strict';
+  /**
+   * Holds the value of filterbox
+   * @type {KnockoutObservable<T>}
+   */
   this.filter = ko.observable('');
+  /**
+   * Status of the drawer UI component
+   * @type {KnockoutObservable<T>}
+   */
   this.drawerActive = ko.observable(false);
+  /**
+   * Current venue
+   * @type {KnockoutObservable<T>}
+   */
   this.currentVenue = ko.observable();
+  /**
+   * Status of the list UI component
+   * @type {KnockoutObservable<T>}
+   */
   this.listActive = ko.observable(true);
+  /**
+   * Is triggered when a venue in the list is clicked
+   * @param {Object} clickedVenue The clicked venue
+   */
   this.venueClick = clickedVenue => {
     this.toggleListAndDrawer();
     this.setCurrentVenue(clickedVenue);
   };
+  /**
+   * Toggle venues list and venue details drawer
+   * to active/non-active.
+   * Only 1 should be shown at a time
+   */
   this.toggleListAndDrawer = () => {
     this.drawerActive(!this.drawerActive());
     this.listActive(!this.listActive());
   };
+  /**
+   * Set currentVenue to venue
+   * @param {Object} venue Venue object
+   * @see http://knockoutjs.com/documentation/click-binding.html
+   */
   this.setCurrentVenue = venue => {
     'use strict';
     this.currentVenue(venue);
   };
+  /**
+   * Initialize the array
+   * @type {KnockoutObservableArray<T>}
+   */
   this.venues = ko.observableArray([]);
+  /**
+   * Filtering function
+   * @type {KnockoutComputed<T>}
+   */
   this.venuesToShow = ko.pureComputed(function() {
     var filter = this.filter();
     if (filter === '') {
       return this.venues();
     }
+    /**
+     * Filter through array and return only matching results
+     * @see http://knockoutjs.com/examples/animatedTransitions.html
+     */
     return ko.utils.arrayFilter(this.venues(), function(venue) {
       // TODO: Make filtering UX MUCH better
       return venue.category.tag === filter ||
