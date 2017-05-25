f6fcb2324426f38be749641515d104aebe6df60b
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-25 21:47:20 +0300

    refactor: Change init map center

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -83,7 +83,7 @@
   };
   window.initMap = function() {
     window.map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 40.7413549, lng: -73.99802439999996},
+      center: {lat: 38.464576, lng: 23.600043},
       zoom: 13,
       // Style by: Sam Herbert, on: https://snazzymaps.com/style/44/mapbox
       styles: [
