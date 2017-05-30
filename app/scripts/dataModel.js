c54f36c2d8a8a3ed884fc1a14ed33adb869a8aca
Konstantinos Dimakis <dimakisblogging@gmail.com>
2017-05-30 14:06:31 +0300

    refactor: Remove self invoking function@dataModel

--- app/scripts/dataModel.js
+++ app/scripts/dataModel.js
@@ -1,89 +1,86 @@
 var dataModel = {};
-(function() {
-  'use strict';
-  dataModel.foursquare = new Promise(function(resolve, reject) {
-    const FOURSQUARE_CLIENT_ID = 'DZLWV3OVTRDIJ4A3HYBWIHZLHNPPWSPULWU0MOIUPV4UGD0B';
-    const FOURSQUARE_CLIENT_SECRET = 'MFMOIF4BD4H0K3ULA41LF2WD5SK0KRJAQLTUZT4VV3XVGC0O';
-    // here I can replace this static address for navigator.geolocation
-    const LL = '38.464576,23.600043';
-    const V = '20170425';
-    const VENUE_PHOTOS = '1';
-    const BASE_URL = 'https://api.foursquare.com/v2/venues/explore?';
-    const LIMIT = 50;
+dataModel.foursquare = new Promise(function(resolve, reject) {
+  const FOURSQUARE_CLIENT_ID = 'DZLWV3OVTRDIJ4A3HYBWIHZLHNPPWSPULWU0MOIUPV4UGD0B';
+  const FOURSQUARE_CLIENT_SECRET = 'MFMOIF4BD4H0K3ULA41LF2WD5SK0KRJAQLTUZT4VV3XVGC0O';
+  // here I can replace this static address for navigator.geolocation
+  const LL = '38.464576,23.600043';
+  const V = '20170425';
+  const VENUE_PHOTOS = '1';
+  const BASE_URL = 'https://api.foursquare.com/v2/venues/explore?';
+  const LIMIT = 50;
 
-    var url = BASE_URL +
-      'client_id=' + FOURSQUARE_CLIENT_ID +
-      '&client_secret=' + FOURSQUARE_CLIENT_SECRET +
-      '&ll=' + LL +
-      '&v=' + V +
-      '&venuePhotos=' + VENUE_PHOTOS +
-      '&limit=' + LIMIT;
+  var url = BASE_URL +
+    'client_id=' + FOURSQUARE_CLIENT_ID +
+    '&client_secret=' + FOURSQUARE_CLIENT_SECRET +
+    '&ll=' + LL +
+    '&v=' + V +
+    '&venuePhotos=' + VENUE_PHOTOS +
+    '&limit=' + LIMIT;
 
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'json';
 
-    request.onload = function() {
-      if (request.status === 200) {
-        // If successful, resolve the promise by passing back the venues array
-        var venues = [];
-        for (let item of request.response.response.groups[0].items) {
-          let venue = {
-            name: item.venue.name,
-            foursquare: 'https://foursquare.com/v/' + item.venue.id + '?ref=' + FOURSQUARE_CLIENT_ID,
-            location: {lat: item.venue.location.lat, lng: item.venue.location.lng},
-            distance: item.venue.location.distance,
-            address: item.venue.location.address,
-            rating: {
-              number: Math.round(item.venue.rating) / 2,
-              stars: [
-                'images/star_border_yellow_18px.svg',
-                'images/star_border_yellow_18px.svg',
-                'images/star_border_yellow_18px.svg',
-                'images/star_border_yellow_18px.svg',
-                'images/star_border_yellow_18px.svg'
-              ]
-            },
-            icon: item.venue.categories[0].icon.prefix + 'bg_32' + item.venue.categories[0].icon.suffix,
-            category: {
-              name: item.venue.categories[0].name,
-              tag: item.venue.categories[0].shortName
-            },
-            photo: item.venue.featuredPhotos.items[0].prefix + 'original' + item.venue.featuredPhotos.items[0].suffix,
-          };
-          // fix rating.stars
-          var i;
-          for (i = 0; i < Math.floor(venue.rating.number); i++) {
-            venue.rating.stars[i] = 'images/star_yellow_18px.svg';
-          }
-          if (venue.rating.number % 1 !== 0) {
-            venue.rating.stars[i] = 'images/star_half_yellow_18px.svg';
-          }
-          // filter through non-standard information
-          venue.phone = item.venue.contact.phone ? item.venue.contact.phone : null;
-          venue.facebook = item.venue.contact.facebook ? 'https://www.facebook.com/' + item.venue.contact.facebook : null;
-          venue.price = item.venue.price ? item.venue.price.message : null;
-          // Even if Foursquare has hours on a venue,
-          // when a venue is closed sometimes the status
-          // message is omitted and only the flag
-          // isOpen: false exists. That's the logic
-          // the following statement represents
-          venue.status = item.venue.hours ? item.venue.hours.status || 'Closed' : null;
-          venue.url = item.venue.url ? item.venue.url : null;
-          venues.push(venue);
+  request.onload = function() {
+    if (request.status === 200) {
+      // If successful, resolve the promise by passing back the venues array
+      var venues = [];
+      for (let item of request.response.response.groups[0].items) {
+        let venue = {
+          name: item.venue.name,
+          foursquare: 'https://foursquare.com/v/' + item.venue.id + '?ref=' + FOURSQUARE_CLIENT_ID,
+          location: {lat: item.venue.location.lat, lng: item.venue.location.lng},
+          distance: item.venue.location.distance,
+          address: item.venue.location.address,
+          rating: {
+            number: Math.round(item.venue.rating) / 2,
+            stars: [
+              'images/star_border_yellow_18px.svg',
+              'images/star_border_yellow_18px.svg',
+              'images/star_border_yellow_18px.svg',
+              'images/star_border_yellow_18px.svg',
+              'images/star_border_yellow_18px.svg'
+            ]
+          },
+          icon: item.venue.categories[0].icon.prefix + 'bg_32' + item.venue.categories[0].icon.suffix,
+          category: {
+            name: item.venue.categories[0].name,
+            tag: item.venue.categories[0].shortName
+          },
+          photo: item.venue.featuredPhotos.items[0].prefix + 'original' + item.venue.featuredPhotos.items[0].suffix
+        };
+        // fix rating.stars
+        var i;
+        for (i = 0; i < Math.floor(venue.rating.number); i++) {
+          venue.rating.stars[i] = 'images/star_yellow_18px.svg';
         }
-        resolve(venues);
-      } else {
-        // If it fails, reject the promise with an error message
-        reject(Error('There was a problem:', '(', request.status, ')', request.statusText));
+        if (venue.rating.number % 1 !== 0) {
+          venue.rating.stars[i] = 'images/star_half_yellow_18px.svg';
+        }
+        // filter through non-standard information
+        venue.phone = item.venue.contact.phone ? item.venue.contact.phone : null;
+        venue.facebook = item.venue.contact.facebook ? 'https://www.facebook.com/' + item.venue.contact.facebook : null;
+        venue.price = item.venue.price ? item.venue.price.message : null;
+        // Even if Foursquare has hours on a venue,
+        // when a venue is closed sometimes the status
+        // message is omitted and only the flag
+        // isOpen: false exists. That's the logic
+        // the following statement represents
+        venue.status = item.venue.hours ? item.venue.hours.status || 'Closed' : null;
+        venue.url = item.venue.url ? item.venue.url : null;
+        venues.push(venue);
       }
-    };
-    // Also deal with the case when the entire request fails to begin with
-    // This is probably a network error, so reject the promise with an appropriate message
-    request.onerror = function() {
-      reject(Error('Network Error'));
-    };
-    // Execute request
-    request.send();
-  });
-})();
+      resolve(venues);
+    } else {
+      // If it fails, reject the promise with an error message
+      reject(Error('There was a problem:', '(', request.status, ')', request.statusText));
+    }
+  };
+  // Also deal with the case when the entire request fails to begin with
+  // This is probably a network error, so reject the promise with an appropriate message
+  request.onerror = function() {
+    reject(Error('Network Error'));
+  };
+  // Execute request
+  request.send();
+});
